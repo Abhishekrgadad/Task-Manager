4d137f5fa53bbed491aa9b4ab91bb9241622288b
Abhishekrgadad <abhishekgadad485@gmail.com>
2025-01-16 14:17:36 +0530

    Added components

--- frontend/src/app/task/task.component.ts
+++ frontend/src/app/task/task.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task',
-  imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.scss'
+  styleUrls: ['./task.component.css']
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit {
+  tasks: any[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    // Fetch tasks on initialization
+    this.taskService.getTasks().subscribe(data => {
+      this.tasks = data;
+    });
+  }
+
+  // Add a task
+  addTask(title: string): void {
+    const newTask = { title, completed: false };
+    this.taskService.addTask(newTask).subscribe(task => {
+      this.tasks.push(task);
+    });
+  }
+
+  // Delete a task
+  deleteTask(id: string): void {
+    this.taskService.deleteTask(id).subscribe(() => {
+      this.tasks = this.tasks.filter(task => task._id !== id);
+    });
+  }
+
+  // Toggle task completion status
+  toggleCompletion(id: string): void {
+    const taskToUpdate = this.tasks.find(task => task._id === id);
+    taskToUpdate.completed = !taskToUpdate.completed;
+    this.taskService.updateTask(id, taskToUpdate).subscribe();
+  }
 }
